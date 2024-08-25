635013b94266cc586a43368fa981163c732e45d0
Drag_shreyas <drag.shreyas@gmail.com>
2024-08-25 19:28:26 +0530

    org-sidebar

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -13,17 +13,17 @@ export const Navbar = () => {
                 <div className="hidden md:flex">
                     <Logo />
                 </div>
-                <Button size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
                     Create
                 </Button>
-                <Button size="sm" className="rounded-sm block md:hidden">
+                <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
                     <Plus className="h-4 w-4"/>
                 </Button>
             </div>
 
             <div className="ml-auto flex items-center gap-x-2">
             <div>                
-                <Button size="sm" className="rounded-sm flex items-center justify-between bg-white py-1.5 px-2 ">
+                <Button variant="primary" size="sm" className="rounded-sm flex items-center justify-between bg-white py-1.5 px-2 ">
                     <ChatLogo />
                 </Button>
             </div>
