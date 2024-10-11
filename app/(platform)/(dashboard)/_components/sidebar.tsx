d3b9ff91a8bdb3c9ec4f39d3fa026f593590fc4f
Drag_shreyas <drag.shreyas@gmail.com>
2024-10-11 19:49:54 +0530

    sidebar

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,15 +1,81 @@
 "use client";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization , useOrganizationList } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+
 
 interface SidebarProps {
     storageKey?: string;
 }
 
 export const Sidebar = ({
-    storageKey,
+    storageKey = "pal-sidebar-state",
 }: SidebarProps) => {
+    const [expanded, setExpanded] = useLocalStorage<Record<string,any>>(
+        storageKey,
+        {}
+    );
+    const {
+        organization: activeOrganization,
+        isLoaded: isLoadedOrg,
+    } = useOrganization();
+    const{
+        userMemberships,
+        isLoaded: isLoadedOrgList
+    } = useOrganizationList({
+        userMemberships:{
+            infinite: true,
+        },
+    });
+
+    const defaultAccordianValue: string[] = Object.keys(expanded).reduce((acc: string[],key: string)=> {
+        if (expanded[key]) {
+            acc.push(key);
+        }
+        return acc;
+    }, []);
+    
+    const onExapand = (id: string) => {
+        setExpanded((curr) => ({
+            ...curr,
+            [id]: !expanded[id],
+        }));
+    }; 
+
+    if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+        return (
+            <>
+            <Skeleton />
+            </>
+        );
+    }
+
+
     return (
-        <div>
-            Sidebar!
-        </div>
+        <>
+            <div className="font-medium text-xs flex items-center mb-1">
+                <span className="pl-4">
+                    Workspaces
+                </span>
+                <Button 
+                asChild
+                type="button"
+                size="icon"
+                variant="ghost"
+                className="ml-auto"
+                >
+                    <Link href="/select-org">
+                    <Plus className="h-4 w-4" />
+                    </Link>
+                </Button>
+            </div>
+        </>
     );
 };
